97a08080e30efad14f10a7fafc238800c011b8ba
Abhinav Tyagi <Abhinavtyagiup@gmail.com>
2024-05-25 10:45:59 +0530

    / Page loding resolved

--- src/App.js
+++ src/App.js
@@ -33,16 +33,17 @@ export default class App extends Component {
        
       />
       <Routes>
+          
+          <Route setProgress = {this.setProgress}exact path="/" element={<NewsComponent  key="differe" title="Title" country="in" category="general"/>}></Route>
+
           <Route setProgress = {this.setProgress}exact path="/business" element={<NewsComponent key="different" title="Title" country="in" category="business"/>}></Route>
           <Route setProgress = {this.setProgress}exact path="/entertainment" element={<NewsComponent key="differen" title="Title" country="in" category="entertainment"/>}></Route>
-          <Route setProgress = {this.setProgress}exact path="/general" element={<NewsComponent  key="differe" title="Title" country="in" category="general"/>}></Route>
           <Route setProgress = {this.setProgress}exact path="/health" element={<NewsComponent key="differ" title="Title" country="in" category="health"/>}></Route>
           <Route setProgress = {this.setProgress}exact path="/science" element={<NewsComponent key="diffe" title="Title" country="in" category="science"/>}></Route>/general
           <Route setProgress = {this.setProgress}exact path="/sports" element={<NewsComponent key="diff" title="Title" country="in" category="sports"/>}></Route>
           <Route setProgress = {this.setProgress}exact path="/technology" element={<NewsComponent key="dif" title="Title" country="in" category="technology"/>}></Route>
         
       </Routes>
-      {/* </BrowserRouter> */}
       
       
       </div>
